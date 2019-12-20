1fe28528eee845b11fd3ff8c37c3c6039ff4cead
Christoff Steenkamp <54899515+JacX808@users.noreply.github.com>
2019-12-20 22:52:33 +0200

    3 Scripts for reference

---
+++ AimControl.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AimControl : MonoBehaviour
+{
+    [Header("---- System ----")]
+    public GameObject playerGun;
+    public GameObject playerAvatar;
+
+   [SerializeField]
+    protected float mouseDist = 2f; // default 2m
+
+    [SerializeField]
+    protected GameObject mouseTarget;
+
+    protected bool aimOn = true;
+
+    [Header("---- Visuals ----")]
+    public Image targetImage;
+    public Sprite corsshairSPR;
+   
+
+    // ################################# GETS & SETS
+
+    /// <summary>
+    /// Get set aim status
+    /// </summary>
+    public bool AimOn
+    {
+        get { return aimOn; }
+
+        set { aimOn = value; }
+    }
+
+    // ################################## INI
+    /// <summary>
+    /// 
+    /// </summary>
+    void Start()
+    {
+        
+    }
+
+    // ################################## SYSTEM
+    /// <summary>
+    /// 
+    /// </summary>
+    void FixedUpdate()
+    {
+        UpdateTargetPos();
+    }
+
+    /// <summary>
+    /// Move Gun and crosshair
+    /// </summary>
+    private void UpdateTargetPos()
+    {
+        if(aimOn)
+        {
+            // center pos
+            Vector3 forward = mouseTarget.transform.position - this.transform.position;
+            playerGun.transform.rotation = Quaternion.LookRotation(forward, Vector3.up);
+
+           // Debug.Log("Forward is: " + forward);
+            if(forward.x > 0)
+            {
+                playerAvatar.transform.localEulerAngles = new Vector3(0f, 30f, 0f);
+            }
+            else
+            {
+                playerAvatar.transform.localEulerAngles = new Vector3(0f, 130f, 0f);
+            }
+        }
+    }
+}
---
+++ Bullet.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [Header("---- Audio ----")]
+    [SerializeField]
+    protected AudioClip gunShotA;
+
+    // -- System ---
+    protected float damage = 40f; // defualt 40
+    protected float speed = 12f;
+    private int liveTime = 3;
+    private Vector3 direction;
+
+    // ########################################### GETS & SETS
+    public float BulletSpeed
+    {
+        set{ speed = value; }
+    }
+    public float Damage
+    {
+        set { damage = value; }
+        get { return damage; }
+    }
+
+    // ########################################### INI
+    public void IniBullet(Vector3 _direction, AudioSource sfxSource)
+    {
+        //speed = _speed;
+        //StartCoroutine(Updater(direction));
+        StartCoroutine(LifeTime());
+        direction = _direction;
+        PlayAudio(sfxSource); // play gunshot audio
+    }
+
+    // ########################################## SYSTEM
+
+    void FixedUpdate()
+    {
+       // Debug.Log("Direction: " + direction + " * timeDelta" + Time.deltaTime +  " * speed: " + speed + " = " + (direction * Time.deltaTime * speed));
+        transform.Translate(direction * Time.deltaTime * speed);
+    }
+
+    /// <summary>
+    /// live for a time and then destroy this obj
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator LifeTime()
+    {
+        float count = 0f;
+        while (count < liveTime)
+        {
+            count += 0.02f;
+            yield return new WaitForSeconds(0.03f);
+        }
+
+        Destroy(this.gameObject);
+    }
+
+    private void OnTriggerEnter(Collider col) // bullet collides
+    {
+        if(col.tag == "FloorSpawn") // hit wall or floor
+        {
+            // play sound on hit
+            Destroy(this.gameObject);
+        }
+
+        if (col.tag == "NPC") // hit NPC
+        {
+            //Debug.Log("Hit NPC : " + damage);
+            EnemyNPC npc = col.GetComponent<EnemyNPC>();
+            npc.GetShot(damage);
+
+            Destroy(this.gameObject);
+        }
+
+        if(col.tag == "Player")
+        {
+           // Debug.Log("Player hit");
+            PlayerManager play = col.GetComponent<PlayerManager>();
+            play.TakeDMG(damage);
+
+            Destroy(this.gameObject);
+        }
+    }
+
+    /// <summary>
+    /// Play gunshot audio 
+    /// </summary>
+    private void PlayAudio(AudioSource sfxSource)
+    {
+        sfxSource.clip = gunShotA;
+        sfxSource.Play();
+    }
+}
